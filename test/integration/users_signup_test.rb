d24a8520bdd1eb080fa00e7cda9ea754852cd95b
jorge-allende <jorgeaallendela@gmail.com>
2023-12-02 12:25:15 -0500

    Update users_signup_test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,7 +7,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: '', email: "user@invalid", password: "foo", password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
+    assert_template 'users/new'  # Update this line to match your signup form template
+    assert_select 'div#error_explanation'  # Optional: Assert specific error messages
+    # ... (any other assertions related to error messages or form re-rendering)
   end
 end
